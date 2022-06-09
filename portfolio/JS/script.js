7c4a75c08a3c18d1c061536ff265f0c527a34429
gioverdiansyah <gioverdiansyh@gmail.com>
2022-06-09 16:37:56 +0700

    merapikan code

--- portfolio/JS/script.js
+++ portfolio/JS/script.js
@@ -3,14 +3,14 @@ const menuToggle = document.querySelector('.menu-toggle input');
 const header = document.querySelector('header ul');
 
 menuToggle.addEventListener('click', function() {
-header.classList.toggle('slide');
+    header.classList.toggle('slide');
 });
 // bagian beranda
 const menuToogle = document.querySelector('.menu-toogle input');
 const nav = document.querySelector('nav ul');
 
 menuToogle.addEventListener('click', function() {
-nav.classList.toggle('slide');
+    nav.classList.toggle('slide');
 });
 
 
@@ -20,10 +20,10 @@ nav.classList.toggle('slide');
 let numbers = document.getElementById('numbers');
 let counters = 0;
 setInterval(() => {
-    if(counters == 30){
+    if (counters == 30) {
         clearInterval();
-    }else{
-    counters +=1;
-    numbers.innerHTML = counters + '%';
+    } else {
+        counters += 1;
+        numbers.innerHTML = counters + '%';
     }
 }, 50);
