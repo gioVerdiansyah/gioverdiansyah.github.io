ad1e65935277a5df78022341328180e3bc29d5e0
gioverdiansyah <gioverdiansyh@gmail.com>
2022-07-21 16:53:59 +0700

    Add parallax effect

--- portfolio/JS/script.js
+++ portfolio/JS/script.js
@@ -1,29 +1,7 @@
-// bagian portfolio
+// bagian portfolio & beranda
 const menuToggle = document.querySelector('.menu-toggle input');
 const header = document.querySelector('header ul');
 
 menuToggle.addEventListener('click', function() {
     header.classList.toggle('slide');
 });
-// bagian beranda
-const menuToogle = document.querySelector('.menu-toogle input');
-const nav = document.querySelector('nav ul');
-
-menuToogle.addEventListener('click', function() {
-    nav.classList.toggle('slide');
-});
-
-
-
-
-
-let numbers = document.getElementById('numbers');
-let counters = 0;
-setInterval(() => {
-    if (counters == 30) {
-        clearInterval();
-    } else {
-        counters += 1;
-        numbers.innerHTML = counters + '%';
-    }
-}, 50);
